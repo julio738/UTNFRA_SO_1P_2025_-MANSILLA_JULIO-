db1c356445819da1664155ffa4723fc99d9a3734
julio738 <mansillajulio.utn@gmail.com>
2026-05-14 02:59:52 +0000

    subiendo punto b correccion

--- Punto_B_FormatMontaje.sh
+++ Punto_B_FormatMontaje.sh
@@ -4,7 +4,7 @@ sudo mount /dev/sdc1 $HOME/Astronomia/Planeta/Gravedad
 sudo mount /dev/sdc2 $HOME/Astronomia/Planeta/Orbita
 sudo mount /dev/sdc5 $HOME/Astronomia/Cometa/Cola
 sudo mount /dev/sdc6 $HOME/Astronomia/Cometa/Gravedad
-sudo mount /dev/sdc7 $HOME/Astronomia/Planeta/Satelite
+sudo mount /dev/sdc7 $HOME/Astronomia/Cometa/Orbita
 
 
 
